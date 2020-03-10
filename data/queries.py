4c534f549be0b96bac405425e1eaac949ee838db
AndreeaNenciuCrasi <aanicaa@yahoo.com>
2020-03-10 11:33:57 +0200

    Adds show.html with show details and actors name

--- data/queries.py
+++ data/queries.py
@@ -12,3 +12,12 @@ def get_first_15_shows():
                                        "LEFT JOIN genres ON show_genres.genre_id = genres.id "
                                        "GROUP BY shows.id "
                                        "ORDER BY shows.rating DESC LIMIT 15;")
+
+def get_show_data(show_id):
+    return data_manager.execute_select(f"SELECT shows.title, shows.year, shows.runtime, shows.trailer, shows.overview, string_agg(DISTINCT genres.name, ',') AS genres FROM shows LEFT JOIN show_genres ON shows.id = show_genres.show_id LEFT JOIN genres ON show_genres.genre_id = genres.id WHERE show_id={show_id} "
+                                       f"GROUP BY  shows.title, shows.year, shows.runtime, shows.trailer, shows.overview;")
+
+def get_show_actors(show_id):
+    return data_manager.execute_select(f"SELECT shows.id, show_characters.character_name, actors.name FROM shows "
+                                       f"LEFT JOIN show_characters ON shows.id = show_characters.show_id "
+                                       f"LEFT JOIN actors ON show_characters.actor_id = actors.id WHERE show_id={show_id};")
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, redirect, request, url_for
 from data import queries
 
 app = Flask('codecool_series')
@@ -14,8 +14,15 @@ def index():
 @app.route('/design')
 def design():
     shows = queries.get_first_15_shows()
-
     return render_template('design.html', shows=shows)
+
+
+@app.route('/details/<show_id>')
+def display_show_details(show_id):
+    shows = queries.get_first_15_shows()
+    show_details = queries.get_show_data(show_id)
+    show_actors = queries.get_show_actors(show_id)
+    return render_template('show.html', shows=shows, show_details=show_details, show_actors=show_actors)
 
 
 def main():
